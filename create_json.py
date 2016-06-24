bbcd8f71afa3958405f7017890d5fc9ec8d0d4cd
zylphrex <zylphrex@gmail.com>
2016-06-23 23:29:13 -0400

    further adjustments to parsing

--- create_json.py
+++ create_json.py
@@ -8,7 +8,12 @@ from courspider.faculty_calendar_resources.session import Session
 from courspider.faculty_calendar_resources.session import Season
 from courspider.faculty_calendar_resources.url import URL
 
-def create_course_json():
+
+def write_data(data, dest):
+    file = open(dest, 'w')
+    file.write(json.dumps(data, indent=4))
+
+def _create_course_data():
     cal = FacultyOfArtsAndScienceCalendar(Session(2016, Season.FALL),
                                           URL("http://calendar.artsci.utoronto.ca"))
 
@@ -22,16 +27,28 @@ def create_course_json():
         for course in dep.get_courses():
             course_datas.append(course.to_data())
 
-    file = open("data/courses.json", 'w')
-    file.write(json.dumps({"courses": course_datas}, indent=4))
+    return {"courses": course_datas}
+
+def create_course_json():
+    write_data(_create_course_data(), "data/courses.json")
     print('course data written')
 
-def create_department_json():
+def _create_department_data():
     cal = FacultyOfArtsAndScienceCalendar(Session(2016, Season.FALL),
                                           URL("http://calendar.artsci.utoronto.ca"))
     print('getting department calendars')
     departments = [department.name for department in cal.get_departments()]
 
-    file = open("data/departments.json", 'w')
-    file.write(json.dumps({"departments": departments}, indent=4))
+    return {"departments": departments}
+
+def create_department_json():
+    write_data(_create_department_data(), "data/departments.json")
     print('department data written')
+
+def create_data_json():
+    courses = _create_course_data()
+    departments = _create_department_data()
+    distribution_requirements = {"distribution_requirements": ["Humanity", "Science", "Social Science"]}
+    breadth_requirements = {"breadth_requirements": ["1. Creative and Cultural Representations", "2. Thought, Belief, and Behaviour", "3. Society and Its Institutions", "4. Living Things and Their Environment", "5. The Physical and Mathematical Universes"]}
+    data = {**courses, **departments, **distribution_requirements, **breadth_requirements}
+    write_data(data, "data/data.json")
